b5383209d0bad561546b463932369e8d1bb14bf6
Robin <dtr.robin09@gmail.com>
2020-09-28 00:40:42 +0200

    added variables

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val quizQuestions = QUIZ_QUESTIONS.toMutableList()
+    private val questionAdapter = QuestionAdapter(quizQuestions)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
